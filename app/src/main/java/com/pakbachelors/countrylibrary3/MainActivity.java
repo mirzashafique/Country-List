c2f3b26d704389f2c4b044782da769b287564cc4
Pak Bachelors <mirza.shf121@gmail.com>
2019-10-07 05:18:58 +0500

    aaaa

--- app/src/main/java/com/pakbachelors/countrylibrary3/MainActivity.java
+++ app/src/main/java/com/pakbachelors/countrylibrary3/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +13,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.pakbachelors.countrylist.CountryList;
 import com.pakbachelors.countrylist.model.Country;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private ImageView backImage;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        backImage = findViewById(R.id.back_im);
+        view = findViewById(R.id.view);
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
                 CountryList.showList(MainActivity.this);
             }
         });
+
+        Locale locale = new Locale("ar");
+        Locale.setDefault(locale);
+        if (isRTL(Locale.getDefault())) {
+            backImage.setImageResource(R.drawable.ic_right_arrow);
+            view.setBackgroundResource(R.drawable.ic_shape_right);
+        } else {
+            backImage.setImageResource(R.drawable.ic_back_arrow);
+            view.setBackgroundResource(R.drawable.ic_shape_blue);
+        }
 
     }
 
@@ -41,5 +58,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), country.getCountryName(), Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    public static boolean isRTL(Locale locale) {
+        final int directionality = Character.getDirectionality(locale.getDisplayName().charAt(0));
+        return directionality == Character.DIRECTIONALITY_RIGHT_TO_LEFT ||
+                directionality == Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC;
     }
 }
